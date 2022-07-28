69b793816dad6cd63119b69e6bb894333b271ae4
devilhyt <devilhyt@gmail.com>
2022-07-28 21:37:18 +0800

    update

--- runserver.py
+++ runserver.py
@@ -1,18 +1,10 @@
 from wingman_api import app
-from flask import request
 
 if __name__ == '__main__':
     
     # test page
     @app.get("/")
     def hello_world_get():
-        print(request.args.get('mode'))
-        return "<p>Hello, World!</p>"
-
-    @app.post("/")
-    def hello_world_post():
-        print(request.get_json())
-        print(request.args.get('mode'))
         return "<p>Hello, World!</p>"
     
     app.run(host="0.0.0.0", debug=True)
