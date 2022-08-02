55272674955d67a22e2a272acd433b0fc22f5539
devilhyt <devilhyt@gmail.com>
2022-08-02 16:00:41 +0800

    update

--- run.py
+++ run.py
@@ -7,4 +7,11 @@ if __name__ == '__main__':
     def hello_world_get():
         return "<p>Hello, World!</p>"
     
+    from flask import request
+    @app.post("/<string:test_string>")
+    def hello_world_post(test_string):
+        print(request.view_args)
+        print(request.json)
+        return "<p>Hello, World!</p>"
+    
     app.run(host="0.0.0.0", debug=True)
