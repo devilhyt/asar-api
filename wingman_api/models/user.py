2f7ea9d2131f6eb931db7ba139cf2733af50b63a
devilhyt <devilhyt@gmail.com>
2022-08-11 06:04:40 +0800

    update

--- wingman_api/models/user.py
+++ wingman_api/models/user.py
@@ -2,15 +2,18 @@ from typing import Optional
 from pydantic import BaseModel, Extra
 from wingman_api.extensions import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Text, nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
-    
+
+
 class UserSchema(BaseModel, extra=Extra.forbid):
-    id : Optional[int]
-    username : str
-    password : str
+    id: Optional[int]
+    username: str
+    password: str
+
 
 def init():
     db.session.add(User(username="admin", password="admin"))
