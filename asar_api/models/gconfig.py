0367f8ebf8705d38dfa701de63815ff669d836f9
devilhyt <devilhyt@gmail.com>
2022-09-24 11:36:46 +0800

    add compile method

--- asar_api/models/gconfig.py
+++ asar_api/models/gconfig.py
@@ -1,18 +1,22 @@
 from typing import Optional
 from pydantic import BaseModel, Field
 import json
+from ruamel.yaml import YAML
 from pathlib import Path
 from ..config import ASAR_DATA_ROOT, GCONFIG_FILE_NAME
+
 
 class GConfig():
     def __init__(self) -> None:
         self.file = Path(ASAR_DATA_ROOT).joinpath(GCONFIG_FILE_NAME)
         self.object_schema = GConfigSchema
+        # Tools
+        self.yaml = YAML()
 
     @property
     def content(self) -> dict:
         return self.read_json()
-    
+
     @property
     def names(self) -> tuple:
         return tuple(self.content.keys())
@@ -21,6 +25,7 @@ class GConfig():
         if not self.file.exists():
             self.file.touch()
             self.file.write_text(GConfigSchema().json(by_alias=True, indent=4))
+            self.compile()
 
     def update(self, input_content) -> None:
         # Validate
@@ -28,6 +33,19 @@ class GConfig():
         # Implement
         content = valid_content.dict(by_alias=True)
         self.write_json(content)
+        self.compile()
+
+    def compile(self) -> None:
+        # Todo: optimize required
+        content = self.content
+        with open(file=Path(ASAR_DATA_ROOT).joinpath("credentials.yml"),
+                  mode='w',
+                  encoding="utf-8") as y:
+            self.yaml.dump(data=content["credentials"], stream=y)
+        with open(file=Path(ASAR_DATA_ROOT).joinpath("endpoints.yml"),
+                  mode='w',
+                  encoding="utf-8") as y:
+            self.yaml.dump(data=content["endpoints"], stream=y)
 
     def read_json(self) -> dict:
         with open(self.file, 'r', encoding="utf-8") as f:
@@ -38,11 +56,13 @@ class GConfig():
         with open(self.file, 'w', encoding="utf-8") as f:
             json.dump(f_json, f, indent=4, ensure_ascii=False)
 
+
 class DockerSchema(BaseModel):
     rasa_container: str = 'app'
     action_container: str = 'action'
     asar_api_url: str = 'http://localhost:5500'
     rasa_api_url: str = 'http://localhost:5005'
+
 
 class RasaCredentialsSchema(BaseModel):
     url: str = "http://localhost:5002/api"
