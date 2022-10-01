79c831a9de889cfe5b726d9c5ef089d8d2530810
devilhyt <devilhyt@gmail.com>
2022-10-02 00:21:09 +0800

    update train(), load()

--- asar_api/controller/model.py
+++ asar_api/controller/model.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request
 from flask.views import MethodView
 from flask_jwt_extended import jwt_required
 from ..models.project import Project
-import os
 
 class ModelAPI(MethodView):
     """Asar Model API"""
@@ -13,16 +12,13 @@ class ModelAPI(MethodView):
         # Implement
         prj = Project(project_name)
         prj.compile()
-        rasa_api_url = os.getenv("RASA_API_URL")
-        asar_api_url = os.getenv("ASAR_API_URL")
         
-        if rasa_api_url and asar_api_url:
-            status_code=200
-            # Todo: uncomment
-            # status_code = prj.models.train(rasa_api_url=rasa_api_url,asar_api_url=asar_api_url)
-            return jsonify({'rasa_status_code': status_code}), 200
-        else:
-            return jsonify({'msg': 'RASA_API_URL or ASAR_API_URL environment variable not found'}), 400                                       
+        status_code=200
+        rasa_status_code=200
+        msg = "test mode"
+        # Todo: uncomment
+        # status_code, rasa_status_code, msg = prj.models.train()
+        return jsonify({'rasa_status_code': rasa_status_code, 'msg': msg}), status_code                                     
         
 
     def post(self, project_name):
@@ -36,12 +32,13 @@ class ModelAPI(MethodView):
     @jwt_required()
     async def put(self, project_name):
         prj = Project(project_name)
-        rasa_api_url = os.getenv("RASA_API_URL")
-        if rasa_api_url:
-            status_code = prj.models.load(rasa_api_url=rasa_api_url)
-            return jsonify({'rasa_status_code': status_code}), 200
-        else:
-            return jsonify({'msg': 'RASA_API_URL environment variable not found'}), 400
+
+        status_code=200
+        rasa_status_code=200
+        msg = "test mode"
+        # Todo: uncomment
+        # status_code, rasa_status_code, msg = prj.models.load()
+        return jsonify({'rasa_status_code': rasa_status_code, 'msg': msg}), status_code
 
     @classmethod
     def init_app(cls, app: Flask):
--- asar_api/models/model.py
+++ asar_api/models/model.py
@@ -1,47 +1,64 @@
 from pathlib import Path
 import requests
 from ruamel.yaml import YAML
-from ..config import OUTPUT_DIR_NAME, MODELS_FILE_NAME
+from ..config import OUTPUT_DIR_NAME
+import os
 
 
 class Model:
     def __init__(self,
                  prj_path: Path,
                  prj_name: str,
-                 dir_name: str = OUTPUT_DIR_NAME,
-                 file_name: str = MODELS_FILE_NAME) -> None:
+                 dir_name: str = OUTPUT_DIR_NAME) -> None:
         self.prj_name = prj_name
         self.dir = prj_path.joinpath(dir_name)
-        self.file = self.dir.joinpath(file_name)
+        self.file = self.dir.joinpath(f'{prj_name}.tar.gz')
         # Tools
         self.yaml = YAML(typ='safe')
+        # env var
+        self.env_rasa_api_url = os.getenv("RASA_API_URL")
+        self.env_asar_api_url = os.getenv("ASAR_API_URL")
 
     def init(self) -> None:
         self.dir.mkdir(parents=True)
 
-    def train(self, rasa_api_url, asar_api_url) -> None:
-        params = {'save_to_default_model_directory': 'false',
-                  'force_training': 'true',
-                  'callback_url': f'{asar_api_url}/projects/{self.prj_name}/models'}
+    def train(self, custom_rasa_api_url=None, custom_asar_api_url=None) -> None:
+        rasa_api_url = custom_rasa_api_url or self.env_rasa_api_url
+        asar_api_url = custom_asar_api_url or self.env_asar_api_url
+        
+        if rasa_api_url and asar_api_url:
+            params = {'save_to_default_model_directory': 'false',
+                    'force_training': 'true',
+                    'callback_url': f'{asar_api_url}/projects/{self.prj_name}/models'}
+            
+            # response = requests.get(url=f'{rasa_api_url}/status')
+            # if response.json().get("num_active_training_jobs") > 0:
+            #     return 400, "Still training"
+            # resp = requests.get(url=f'{rasa_api_url}')
+            # if resp.status_code != requests.codes.ok:
+            #     return resp.status_code
 
-        # response = requests.get(url=f'{rasa_api_url}/status')
-        # if response.json().get("num_active_training_jobs") > 0:
-        #     return 400, "Still training"
+            # Todo: generate yaml from this project
+            with open('./test/test.yml', 'r', encoding="utf-8") as f:
+                data = f.read()
+            response = requests.post(url=f'{rasa_api_url}/model/train',
+                                    params=params,
+                                    data=data)
+            return 200, response.status_code, ""
+        else:
+            return 400, None, "Please provide RASA_API_URL and ASAR_API_URL."
 
-        # Todo: generate yaml from this project
-        with open('./test/test.yml', 'r', encoding="utf-8") as f:
-            data = f.read()
-        response = requests.post(url=f'{rasa_api_url}/model/train',
-                                 params=params,
-                                 data=data)
-        return response.status_code
-
-    def load(self, rasa_api_url) -> None:
-        data = {'model_file': self.file.absolute().as_posix()}
-        response = requests.put(url=f'{rasa_api_url}/model', 
-                                json=data,
-                                timeout=300)
-        return response.status_code
+    def load(self, custom_rasa_api_url=None) -> None:
+        rasa_api_url = custom_rasa_api_url or self.env_rasa_api_url
+        
+        if rasa_api_url:
+            data = {'model_file': self.file.absolute().as_posix()}
+            response = requests.put(url=f'{rasa_api_url}/model', 
+                                    json=data,
+                                    timeout=300)
+            return 200, response.status_code, ""
+        else:
+            return 400, None, "Please provide RASA_API_URL."
 
     def save(self, content) -> None:
         with open(self.file, 'wb') as t:
