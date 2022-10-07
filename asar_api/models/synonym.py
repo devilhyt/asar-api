b4c980c05c0b6e8318abf6b716d9bbce4cc29b28
devilhyt <devilhyt@gmail.com>
2022-10-08 00:44:05 +0800

    add default_content

--- asar_api/models/synonym.py
+++ asar_api/models/synonym.py
@@ -8,7 +8,7 @@ from .file_basis import FileBasis, GeneralNameSchema
 
 class Synonym(FileBasis):
     def __init__(self, prj_path: Path) -> None:
-        self.default_content = {}
+        self.default_content = {'examples': []}
         super().__init__(prj_path=prj_path,
                          file_name=SYNONYMS_FILE_NAME,
                          default_content=self.default_content,
@@ -17,7 +17,7 @@ class Synonym(FileBasis):
     # def compile(self) -> dict:
     #     content = self.content
     #     domain = {'entities': []}
-        
+
     #     for entity_name, entity in content.items():
     #         if entity:
     #             domain['entities'].append({entity_name: entity})
