6d624f349ba9dd3cfc51444ef713d2f86c8bcbec
devilhyt <devilhyt@gmail.com>
2022-10-09 21:07:11 +0800

    MyYAML

--- asar_api/utils/utils.py
+++ asar_api/utils/utils.py
@@ -1,5 +1,16 @@
 from typing import List
+from ruamel.yaml import YAML
+from ruamel.yaml.compat import StringIO
 
+class MyYAML(YAML):
+    def dump(self, data, stream=None, **kw):
+        inefficient = False
+        if stream is None:
+            inefficient = True
+            stream = StringIO()
+        YAML.dump(self, data, stream, **kw)
+        if inefficient:
+            return stream.getvalue()
 
 def decode_story_nodes(input_story: dict) -> dict:
     """get nodes info"""
