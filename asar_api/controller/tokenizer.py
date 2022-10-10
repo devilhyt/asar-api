dd182d510b66fabfc3b6ad30f876246d1ec8bddd
devilhyt <devilhyt@gmail.com>
2022-10-10 17:30:27 +0800

    delim_set

--- asar_api/controller/tokenizer.py
+++ asar_api/controller/tokenizer.py
@@ -7,13 +7,15 @@ from ckip_transformers.nlp import CkipWordSegmenter
 from ..config import ALBERT_BASE_CHINESE_WS_DIR
 from ..utils.utils import convert_words_to_tokens
 
+
 class TokenizerAPI(MethodView):
     """Asar Tokenizer API"""
     decorators = [jwt_required()]
     init_every_request = False
-    
+
     def __init__(self) -> None:
-        self.ws_driver = CkipWordSegmenter(model_name=ALBERT_BASE_CHINESE_WS_DIR)
+        self.ws_driver = CkipWordSegmenter(
+            model_name=ALBERT_BASE_CHINESE_WS_DIR)
 
     def post(self, project_name):
         """tokenize"""
@@ -30,8 +32,11 @@ class TokenizerAPI(MethodView):
         # tokens = [{'token': token, 'start': start, 'end': end}
         #           for (token, start, end) in tokenized]
         if sentence:
-            ws = self.ws_driver([sentence])
-            tokens = convert_words_to_tokens(ws[0], sentence)
+            ws = self.ws_driver(input_text=[sentence],
+                                use_delim=True,
+                                delim_set="，,。：:；;！!？? ")
+            tokenized = [w for w in ws[0] if w and w.strip()]
+            tokens = convert_words_to_tokens(tokenized, sentence)
         else:
             tokens = []
         return jsonify(tokens), 200
@@ -40,5 +45,5 @@ class TokenizerAPI(MethodView):
     def init_app(cls, app: Flask):
         tokenizer_view = cls.as_view('tokenizer_api')
         app.add_url_rule('/projects/<string:project_name>/tokenizer',
-                        view_func=tokenizer_view,
-                        methods=['POST'])
+                         view_func=tokenizer_view,
+                         methods=['POST'])
